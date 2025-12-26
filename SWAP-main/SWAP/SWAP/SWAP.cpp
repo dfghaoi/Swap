6d218fb7331db731bfcc4ad51eb9d9a5244bbf1e
dfghaoi <85017723+dfghaoi@users.noreply.github.com>
2025-12-27 00:00:42 +0100

    Add files via upload

---
+++ SWAP-main/SWAP/SWAP/SWAP.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+int main()
+{
+    int a, b, temp = 0;
+    cout << "A= "; cin >> a;
+    cout << "B= "; cin >> b;
+    cout << "A= " << a << ", B= " << b << endl;
+    temp = a;
+    a = b;
+    b = temp;
+    cout << "A= " << a << ", B= " << b;
+}
